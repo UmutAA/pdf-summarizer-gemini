58171cb35b357ea973048510da89572102ac3de0
UmutAA <201361309+UmutAA@users.noreply.github.com>
2025-10-05 16:22:43 +0300

    Fix PDF summarization with Gemini API

--- summarize.py
+++ summarize.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from google import genai
+import PyPDF2
 
 load_dotenv()
 
@@ -8,9 +9,23 @@ apiKey = os.getenv("GOOGLE_API_KEY")
 
 client = genai.Client(api_key = apiKey)
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash-lite",
-    contents="Explain how AI works in simple terms",
+pdf_path = input("PDF Path Here: ")
+pdf_text = ""
+
+with open(pdf_path, "rb") as f:
+    reader = PyPDF2.PdfReader(f)
+    for page in reader.pages:
+        pdf_text += page.extract_text()
+
+prompt = (
+    "You are a helpful assistant willing to help me learn things with ease. "
+    "Summarize the text below and give me the key words I need to learn:\n\n"
+    + pdf_text
 )
 
-print(response.text)
+response = client.models.generate_content(
+    model="gemini-2.5-flash-lite",
+    contents = prompt
+)
+
+print(response.candidates[0].content)
